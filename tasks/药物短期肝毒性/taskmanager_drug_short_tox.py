beca47665333560d11c6fe05d5b874b684d8cb2c
Antiqueeeee <antiqueserver4@gmail.com>
2024-11-29 18:26:56 +0800

    增加长期短期相关代码

---
+++ tasks/药物短期肝毒性/taskmanager_drug_short_tox.py
@@ -0,0 +1,92 @@
+import os, sys
+current_path = os.path.abspath(os.path.join(__file__, "../"))
+project_path = os.path.abspath(os.path.join(current_path, "../../"))
+sys.path.append(project_path)
+
+import pandas as pd
+import json
+import time
+import requests
+from tasks.taskmanager_abstract import abstract_task_manager
+from tasks.药物短期肝毒性.models.model_logistic_regression import LogisiticRegression, LogisiticRegressionDataProcessor
+from config import *
+
+class task_manager_drug_shot_tox(abstract_task_manager):
+    def __init__(self, task_name) -> None:
+        self.supported_instruction = ["模型推理"]
+        self.supported_model = [
+            "药物短期肝毒性_逻辑回归_20241126_1.pkl"
+        ]
+        super().__init__(task_name)
+        self.model_mapping = {
+            "药物短期肝毒性_逻辑回归_20241126_1.pkl"  : (LogisiticRegression, LogisiticRegressionDataProcessor)
+        }
+        
+    def run_command(self, **kwargs):
+        instruction = kwargs["instruction"]
+        if self.check_instruction_valid(instruction) is False:
+            return {"data" : f'当前任务中支持:{",".join(self.supported_instruction)},{instruction}不在其中。'}
+        
+        if instruction == "模型推理":
+            selected_model, inference_data = kwargs["selected_model"], kwargs["inference_data"]
+            if selected_model not in self.supported_model:
+                raise
+            (manager, precessor) = self.model_mapping[selected_model]
+            data_processor = precessor(task_name=self.task_name)
+            model_manager = manager(task_name=self.task_name, data_processor=data_processor)
+            model_manager.model_load(selected_model)
+            results = model_manager.model_inference(inference_data)
+            self.recorder.info("模型推理完成")
+            response = self.data_upload(results)
+            if response.get("msg", "操作失败") == "操作成功" :
+                self.recorder.info(f"数据同步完成：{self.task_name}-{instruction}-{results}")
+                return {
+                    "data" : f"任务已完成：{self.task_name}-{instruction}-{results}"
+                    ,"UID" : os.path.splitext(kwargs["inference_data"])[0]
+                }
+            else:
+                return {
+                    "data" : f"任务异常：{self.task_name}-{instruction}-{response.get('msg', '服务器未返回Msg')}"
+                    ,"UID" : os.path.splitext(kwargs["inference_data"])[0]
+                }
+    def data_upload(self, result_path):
+        frame = pd.read_excel(result_path)
+        params = list()
+        for param in frame.to_dict("records"):
+            params.append({
+                "checkTime": param["日期"],
+                "sampleSerialNumber": param["样品编号"],
+                "methotrexateEffectivePercentage": float(param["有效性类别1概率"]),
+                "shortTermHepatotoxicityProbability": None,
+                "longTermHepatotoxicityProbability": None,
+                "timestamp": str(int(time.time()))
+            })
+        secret = INTERFACE_SECRET.format(
+            json_data = json.dumps(params, ensure_ascii=False)
+            ,timestamp = str(int(time.time()))
+            ,nonce = 9527
+            ,key = SECRETKEY
+        )
+        request_body = {
+            "data" : json.dumps(params, ensure_ascii=False),
+            "nonce" : 9527,
+            "timestamp" : str(int(time.time())),
+            "signStr" : self.md5_encoding(secret)
+        }
+        response = requests.post(INTERFACE_GENE_DATA_UPLOAD, json=request_body).json()
+        return response
+        
+if __name__ == "__main__":
+    # task_name = "药物有效性"
+    # tm = taskmanager_drug_efficacy(task_name)
+    # kwargs = {
+    #     "task_name" : task_name
+    #     ,"instruction" : "模型推理"
+    #     ,"selected_model" : "药物有效性_逻辑回归_20241126_1.pkl"
+    #     ,"inference_data" : "20241126_检测结果.xlsx"
+    # }
+    # tm.run_command(**kwargs)
+    
+    task_name = "药物短期肝毒性"
+    tm = task_manager_drug_shot_tox(task_name)
+    # tm.data_upload(r"E:\feynmindPyhton\warmup_torch\tasks\药物有效性\results\_1730796867-20241129.预测结果.xlsx")
---
+++ tasks/药物长期肝毒性/models/model_logistic_regression.py
@@ -0,0 +1,116 @@
+import os,sys
+current_path = os.path.abspath(os.path.join(__file__, "../"))
+project_path = os.path.abspath(os.path.join(current_path, "../../../"))
+sys.path.append(project_path)
+import pickle
+import pandas as pd
+
+from tasks.model_abstract import abstract_model_factory, abstract_data_processor
+from sklearn.impute import SimpleImputer
+
+
+class LogisiticRegressionDataProcessor(abstract_data_processor):
+    def __init__(self, task_name) -> None:
+        super().__init__(task_name)
+
+    def simple_feature_mapping(self, column):
+        origin_data = pd.read_excel(
+            os.path.join(
+                self.path_mananager.get_path("datasets")
+                , "2024.9.12 300人的43SNP基因型疗效肝毒性.xlsx"
+            )
+        )
+        mapping = dict(origin_data[column].value_counts())
+        mapping = {"".join(k.split()): v for k, v in mapping.items()}
+        mapping.pop("00", None)
+        keys_with_set_size_2 = [k for k in mapping if len(set(k)) == 2]
+        filtered_keys = {k: v for k, v in mapping.items() if k not in keys_with_set_size_2}
+        max_value_key = max(filtered_keys, key=filtered_keys.get)
+        min_value_key = min(filtered_keys, key=filtered_keys.get)
+        # Update mapping values
+        for key in keys_with_set_size_2:
+            mapping[key] = 1
+        mapping[max_value_key] = 0
+        mapping[min_value_key] = 2
+        return mapping
+
+    # Apply new mapping to results data
+    def map_value(self, x, mapping):
+        x_key = "".join(x.split())
+        for k, v in mapping.items():
+            if set(x_key) == set(k):
+                return v
+        return 1
+
+class LogisiticRegression(abstract_model_factory):
+    def __init__(self, task_name, data_processor) -> None:
+        super().__init__(task_name, data_processor)
+        self.model = None
+        self.required_features = ['rs2298294', 'rs10937405', 'rs1247456', 'rs11785942', 'rs13051672', 'rs4965177', 'rs4867081', 'rs788970', 'rs9324014', 'rs12513652', 'rs1962292', 'rs2761239', 'rs3888613', 'rs1359168']
+        self.basic_info = [
+            "样品/SNP", "样品编号", "日期"
+        ]
+        
+    def model_load(self, saved_checkpoint):
+        saved_checkpoint_path = os.path.join(
+            self.path_mananager.get_path("checkpoints")
+            , saved_checkpoint
+        )
+        with open(saved_checkpoint_path, "rb") as f:
+            self.model = pickle.load(f)
+        return self.model
+    
+    def model_save(self, model):
+        return super().model_save(model)
+    
+    def model_inference(self, test_file):
+        self.model_load('药物长期肝毒性_逻辑回归_20241126_1.pkl')
+        test_file_name, test_file_fix = os.path.splitext(test_file)
+        _path = os.path.join(self.path_mananager.get_path("datasets"), test_file)
+        self.recorder.info(f"{self.task_name} - 读取数据: {_path}")
+        results_data = pd.read_excel(_path)
+        results_data = results_data.astype(str)
+        results_data = results_data[self.basic_info + self.required_features]
+
+        # 映射
+        for column in results_data.columns:
+            if column in self.required_features:
+                mapping = self.data_processor.simple_feature_mapping(column)
+                results_data[column] = results_data[column].apply(lambda x: self.data_processor.map_value(x, mapping))
+        _path = os.path.join(
+                self.path_mananager.get_path("results")
+                , f"_{test_file_name}.映射结果{test_file_fix}"
+            )
+        self.recorder.info(f"{self.task_name} - 映射结果存储位置为: {_path}")
+        results_data.to_excel(_path, index=False)
+        
+        # 开始推理
+        _data = results_data[self.required_features]
+        imputer = SimpleImputer(strategy='most_frequent')
+        _data = pd.DataFrame(imputer.fit_transform(_data), columns=_data.columns)
+        _data = _data.astype(float)
+        predictions = self.model.predict(_data)
+        probabilities = self.model.predict_proba(_data)[:, 1]
+        prediction_results = pd.DataFrame({
+            '样品编号': results_data['样品编号'],  # 使用共同的键
+            f'长期肝毒性类别': predictions,
+            f'长期肝毒性类别1概率': probabilities
+        })
+        results_with_info = pd.merge(results_data, prediction_results, on='样品编号')
+        _path = os.path.join(
+                self.path_mananager.get_path("results")
+                , f"_{test_file_name}.预测结果{test_file_fix}"
+            )
+        results_with_info.to_excel(_path, index=False)
+        self.recorder.info(f"{self.task_name} - 预测结果存储位置为: {_path}")
+        return _path
+        
+    def model_training(self, model):
+        return super().model_training(model)
+    
+if __name__ == "__main__":
+    task_name = "药物长期肝毒性"
+    data_prcessor = LogisiticRegressionDataProcessor(task_name=task_name)
+    test_file = "1730796867-20241129.xlsx"
+    lr = LogisiticRegression(task_name=task_name,data_processor=data_prcessor)
+    lr.model_inference(test_file=test_file)
